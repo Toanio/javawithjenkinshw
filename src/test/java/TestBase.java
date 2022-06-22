37b656767f02f07e5ac09d9dbad4535967200388
Toanio <c.toan@corp.mail.ru>
2022-06-22 17:25:48 +0300

    Добавил лиснер

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -1,4 +1,6 @@
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -8,6 +10,9 @@ import static com.codeborne.selenide.Selenide.closeWebDriver;
 public class TestBase {
     @BeforeAll
     static void setUp() {
+
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
         Configuration.holdBrowserOpen = true;
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "4000x2000";
