f8cac6497ac06433ee9253e0628b26cbc3ff833e
Toanio <c.toan@corp.mail.ru>
2022-06-23 18:02:31 +0300

    Домашняя работа 11

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -1,22 +1,26 @@
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import properties.CredentialsConfig;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 public class TestBase {
+
+    static CredentialsConfig config = ConfigFactory.create(CredentialsConfig.class);
     @BeforeAll
     static void setUp() {
 
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
 
         Configuration.holdBrowserOpen = true;
-        Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "4000x2000";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.baseUrl = System.getProperty("base_url"); //"https://demoqa.com"
+        Configuration.browserSize = System.getProperty("browser_size"); //"4000x2000";
+        Configuration.remote = "https://"+ config.login() + ":" + config.password() + "@" + System.getProperty("server_selenoid");
 
         DesiredCapabilities capabilities = new DesiredCapabilities(); // набор ключей и значений
         capabilities.setCapability("enableVNC", true); // трнаслировать видео - тру
